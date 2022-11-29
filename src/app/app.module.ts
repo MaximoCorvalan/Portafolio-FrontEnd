f773c9edc27b737a31c96c3049aa99c196f0f822
MaximoCorvalan <corvalanmaximo@gmail.com>
2022-11-29 18:34:45 -0300

    Ap-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercademiComponent } from './components/acercademi/acercademi.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { FomacionComponent } from './components/fomacion/fomacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { IdiomasComponent } from './components/idiomas/idiomas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AcercademiComponent,
+    ExperienciaLaboralComponent,
+    FomacionComponent,
+    SkillsComponent,
+    IdiomasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/fomacion/fomacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fomacion',
+  templateUrl: './fomacion.component.html',
+  styleUrls: ['./fomacion.component.css']
+})
+export class FomacionComponent {
+
+}
